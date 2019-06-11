b2c2e4226301286dd1eee3109727464152b2dadb
MOISES <kmsprado2546@gmail.com>
2019-06-11 01:17:48 -0600

    Implementando put y delete

--- tipoexamen2/routes/api/personas.js
+++ tipoexamen2/routes/api/personas.js
@@ -48,10 +48,30 @@ router.post('/', (req, res, next)=>{
 }); //Post
 
 router.put('/:id', (req, res, next)=>{
-    res.status(403).json({"msg" : "No implement"});
+    var id = req.params.id;
+    var modifiedPersona = {};
+    var originalPersona = {};
+
+    personaCollection = personaCollection.map((e, i)=>{
+        if(e.id === id){
+            originalPersona = Object.assign({}, e);
+            return modifiedPersona = Object.assign({}, e, req.body);
+        }
+        return e;
+    }); //map
+    res.status(200).json({o: originalPersona, m: modifiedPersona});
 }); //Put
 
 router.delete('/:id', (req, res, next)=>{
-    res.status(403).json({"msg" : "No implement"});
+    var id = req.params.id;
+    var deletedPersona = {};
+    personaCollection = personaCollection.filter((e, i)=>{
+        if(e.id === id){
+            deletedPersona = Object.assign({}, e);
+            return false;
+        }
+        return true;
+    });//filter
+    res.status(200).json({d:deletedPersona, c: personaCollection});
 });//delete
 module.exports = router;
